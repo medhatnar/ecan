3fd1c45ea2686ce3d1c8a55557ba22bf65d57066
Eric Lau <elau89@gmail.com>
2017-03-06 16:15:29 -0500

    sign up and authorization working:

--- server/database/controllers/UserController.js
+++ server/database/controllers/UserController.js
@@ -8,7 +8,8 @@ module.exports = {
       });
     },
     post: function (req, res) {
-      var params = [req.body.username];
+
+      var params = [req.body.username, req.body.password, req.body.email, 3];
       console.log("WHAT IS THE PARAMS: ", params);
 
       Users.post(params, function(err, results) {
--- server/database/models/UserModel.js
+++ server/database/models/UserModel.js
@@ -4,10 +4,13 @@ module.exports = {
 
     post: function (params, callback) {
       // create a user
-      console.log("WHAT THE PARAMS THOOOOOO: ", params);
+      console.log("PARAMS IN model: ",params);
 
-      var queryStr = 'insert into users(username) values (?)';
+      var queryStr = 'insert into events_manager.users(`username`, `password`, `email`, `groups_id`) values (?, ?, ?, ?)';
+      console.log("QUERY DAT STRANGER THINGS!!:", queryStr);
+
       db.query(queryStr, params, function(err, results) {
+        console.log("err & results", err,results);
         callback(err, results);
     });
   }
